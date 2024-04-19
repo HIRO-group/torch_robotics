a0b24dafeee5e7a397ed605bb9f83618e1aa09b4
Gilberto Briscoe-Martinez <gibr6485@colorado.edu>
2024-04-19 11:12:04 -0600

    fixed hard coded path

--- examples/test_yaml_env.py
+++ examples/test_yaml_env.py
@@ -3,9 +3,10 @@ import pdb
 import torch
 from torch_robotics.visualizers.planning_visualizer import create_fig_and_axes
 import matplotlib.pyplot as plt
+import os
 
 # Specify the path to your YAML file
-yaml_file = "/home/gilberto/mpd-public/deps/torch_robotics/torch_robotics/environments/env_descriptions/env_anuj.yaml"
+yaml_file = os.path.expanduser("~") + "/mpd-public/deps/torch_robotics/torch_robotics/environments/env_descriptions/env_anuj.yaml"
 
 # Instantiate the EnvYaml environment
 tensor_args = {"device": "cpu", "dtype": torch.float32}
